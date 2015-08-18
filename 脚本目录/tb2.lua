6ca8739138abfd4f748b902c73d606ffbccef861
戴均民 <taichunmin@gmail.com>
2015-08-18 14:03:15 +0800

    更新腳本

--- 脚本目录/tb2.lua
+++ 脚本目录/tb2.lua
@@ -92,7 +92,7 @@ function main()
     -- stage end
     repeat
         mSleep(2000);
-        x, y = findImageInRegionFuzzy("/var/touchelf/tb/stage_selection_stamina.bmp", 80, 551, 141, 730, 253, 0xFFFFFF);
+        x, y = findImageInRegionFuzzy("/var/touchelf/tb/stage_selection_stamina.bmp", 90, 583, 173, 690, 226, 0xFFFFFF);
         if x == -1 and y == -1 then
 			touchDown(9, 100, 100);
 			mSleep(60);
--- 脚本目录/tb_find_full_timebar.lua
+++ 脚本目录/tb_find_full_timebar.lua
@@ -2,13 +2,10 @@
 
 -- 主入口函数
 function main()
-    repeat
-        mSleep(2000);
-        x, y = findImageInRegionFuzzy("/var/touchelf/tb/stage_selection_stamina.bmp", 80, 551, 141, 730, 253, 0xFFFFFF);
-        if x == -1 and y == -1 then
-			touchDown(9, 100, 100);
-			mSleep(60);
-			touchUp(9);
-        end
-    until x ~= -1 and y ~= -1;
+	x, y = findImageInRegionFuzzy("/var/touchelf/tb/stage_selection_stamina.bmp", 90, 583, 173, 690, 226, 0xFFFFFF);
+	if x == -1 and y == -1 then
+		notifyMessage('not found', 3000);
+	else
+		notifyMessage('found! ('..x..', '..y..')', 3000);
+	end
 end
